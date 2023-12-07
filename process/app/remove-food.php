0231f562e3fbe7e844e102abc4f0e897b197882f
TharinduNimesh <127307898+TharinduNimesh@users.noreply.github.com>
2023-12-08 02:47:40 +0530

    Remove Food Backend

---
+++ process/app/remove-food.php
@@ -0,0 +1,47 @@
+<?php
+
+include_once("auth.php");
+
+try {
+    # Check request method
+    if ($_SERVER["REQUEST_METHOD"] !== "GET") {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Only GET requests are allowed"
+        ]);
+        exit();
+    }
+
+    if (empty($_GET["id"])) {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Food ID is required"
+        ]);
+        exit();
+    }
+
+    $id = $_GET["id"];
+
+    # Mark food as removed
+    $sql = "UPDATE food SET is_removed = 1 WHERE id = $id";
+    $result = mysqli_query($conn, $sql);
+
+    # Check if query is successful
+    if (!$result) {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Error While Removing Food"
+        ]);
+        exit();
+    }
+
+    echo json_encode([
+        "status" => "success",
+        "message" => "Food Removed Successfully"
+    ]);
+} catch (Exception $e) {
+    echo json_encode([
+        "status" => "error",
+        "message" => $e->getMessage()
+    ]);
+}
