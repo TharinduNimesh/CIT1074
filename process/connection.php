c59c024fa3b2a8d78e22779e6408581ada00d197
TharinduNimesh <127307898+TharinduNimesh@users.noreply.github.com>
2023-12-07 21:09:19 +0530

    Login Backend

---
+++ process/connection.php
@@ -0,0 +1,28 @@
+<?php
+
+# Database credentials
+$DB_HOST = "localhost";
+$DB_USER = "root";
+$DB_PASSWORD = "Well#ON123";
+$DB_NAME = "restaurant";
+
+# Database connection
+try {
+    # Connection
+    $conn = mysqli_connect($DB_HOST, $DB_USER, $DB_PASSWORD, $DB_NAME);
+
+    # Check connection
+    if ($conn->connect_error) {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Database connection failed"
+        ]);
+        exit();
+    }
+} catch (mysqli_sql_exception $e) {
+    # Connection failed
+    echo json_encode([
+        "status" => "error",
+        "message" => "Database connection failed"
+    ]);
+}
---
+++ process/login.php
@@ -0,0 +1,74 @@
+<?php
+
+include_once("connection.php");
+
+try {
+    # Check request method
+    if ($_SERVER["REQUEST_METHOD"] !== "POST") {
+        echo json_encode([
+            "status" => "error",
+            "message" => "Only POST requests are allowed"
+        ]);
+        exit();
+    }
+
+    # Get data from json
+    $data = json_decode(file_get_contents("php://input"));
+
+    # Validate data
+    if (!isset($data->email) || empty($data->password)) {
+        echo json_encode([
+            "status" => "error",
+            "message" => "All fields are required"
+        ]);
+        exit();
+    }
+
+    # Query
+    $sql = "SELECT * FROM user WHERE email = '$data->email'";
+    $result = mysqli_query($conn, $sql);
+
+    # Check if query is successful
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_array($result);
+        $password = $row["password"];
+
+        # Verify password
+        if (!password_verify($data->password, $password)) {
+            echo json_encode([
+                "status" => "error",
+                "message" => "Invalid Login Credentials"
+            ]);
+            exit();
+        }
+
+        # Get user data
+        $email = $row["email"];
+        $name = $row["name"];
+
+        # Set session
+        $_SESSION["user"] = [
+            "id" => $row["id"],
+            "email" => $email,
+            "name" => $name,
+        ];
+
+        echo json_encode([
+            "status" => "success",
+            "message" => "Login Successful",
+        ]);
+        exit();
+    }
+
+    echo json_encode([
+        "status" => "error",
+        "message" => "Invalid Login Credentials",
+    ]);
+    exit();
+} catch (Exception $e) {
+    echo json_encode([
+        "status" => "error",
+        "message" => "Something went wrong"
+    ]);
+    exit();
+}
