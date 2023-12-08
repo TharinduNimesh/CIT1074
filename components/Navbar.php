c07a0f1b2198227a1820a561558d88d8eb07b75d
ChathuraSudaraka <Chathurabro68@gmail.com>
2023-12-08 20:51:27 +0530

    Content

---
+++ components/Navbar.php
@@ -0,0 +1,20 @@
+<header class="header fixed-top d-flex align-items-center justify-content-center rounded">
+    <div class="container-fluid container-xl d-flex align-items-center justify-content-lg-between">
+      <h1 class="logo me-auto me-lg-0 m-0 p-0">
+        <a href="/" class="logo d-flex align-items-center me-auto me-lg-0">
+          <h1>CAFE<span>Eversoft</span>.</h1>
+        </a>
+      </h1>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a href="/">Home</a></li>
+          <li><a href="/pages/about.html">About</a></li>
+          <li><a href="/pages/menu.html">Menu</a></li>
+          <li><a href="/pages/order.html">Orders</a></li>
+          <li><a href="/pages/contact.html">Contact</a></li>
+        </ul>
+      </nav>
+      <a class="btn-menu" href="/pages/login.html">Login Here</a>
+    </div>
+  </header>
